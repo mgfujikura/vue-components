2445d1c5d9a1ed825f0dfab41985acb343364384
FUJIKURA, Tsuyoshi <earlgray@fujikura.biz>
2024-09-03 18:54:25 +0900

    feat: simple, disabled

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <script setup lang="ts">
 import Btn from '@/components/Btn.vue';
+import { ref } from 'vue';
+
+const disabledCheck = ref(false);
 </script>
 
 <template>
   <div>
-    <Btn />
+    <h1>Btn</h1>
+    <h2>simple</h2>
+    <Btn
+      img="/img/gradation-btn.png"
+      img-disabled="/img/nine-slice-btn.png"
+      :width="91"
+      :height="61"
+      :disabled="disabledCheck"
+      >ボタン</Btn
+    >
+    <input
+      type="checkbox"
+      id="disabledCheck"
+      v-model="disabledCheck"
+    /><label for="disabledCheck">disabled</label>
+    <h2>disabled</h2>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,9 +1,52 @@
 <script setup lang="ts">
-//
+import { computed, ref, unref } from 'vue';
+
+const props = defineProps<{
+  img?: string;
+  disabled?: any;
+  imgDisabled?: string;
+  width?: number;
+  height?: number;
+  nineSlice?: {
+    top?: number;
+    bottom?: number;
+    left?: number;
+    right?: number;
+  };
+  shape?: 'square' | 'cupsule';
+  radius?: number;
+}>();
+
+const style = computed(() => {
+  console.log('props', props);
+  const style: any = {};
+  props.img && (style.background = `url(${props.img})`);
+  props.disabled && props.imgDisabled && (style.background = `url(${props.imgDisabled})`);
+  props.width && (style.width = `${props.width}px`);
+  props.height && (style.height = `${props.height}px`);
+  console.log('style', style);
+  return style;
+});
 </script>
 <template>
-  <div>ボタン</div>
+  <button
+    class="FwComponentBtn"
+    :style="style"
+    :disabled="disabled"
+  >
+    <slot></slot>
+  </button>
 </template>
-<style scoped>
-/*  */
+<style>
+button.FwComponentBtn {
+  padding: 0;
+  border: none;
+  outline: none;
+  font: inherit;
+  color: inherit;
+  background: none;
+}
+button.FwComponentBtn:active {
+  background: none;
+}
 </style>
