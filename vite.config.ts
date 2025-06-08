d59062d66f02ebc60901469c244362caa4c24f35
FUJIKURA, Tsuyoshi <earlgray@fujikura.biz>
2025-06-08 17:26:23 +0900

    chore: ビルド調整

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
 import { resolve } from 'path';
-
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), libInjectCss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
